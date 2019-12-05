a8fa370c831d2e01ef0c31b90c499ec60ef10b9b
Shuayb Khan <shuaybk@gmail.com>
2019-12-04 20:02:53 -0500

    Completed S06.01

--- S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -351,6 +351,10 @@ public class MainActivity extends AppCompatActivity implements
         // TODO (2) Set setDisplayHomeAsUpEnabled to true on the support ActionBar
 
         // TODO (6) Launch SettingsActivity when the Settings option is clicked
+        if (id == R.id.action_settings) {
+            Intent launchSettingsActivity = new Intent(this, SettingsActivity.class);
+            startActivity(launchSettingsActivity);
+        }
 
         return super.onOptionsItemSelected(item);
     }
